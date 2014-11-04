eb58d082c750464d53b089f1cb9b815c30ed4062
liuhq002 <yehaha001@163.com>
2014-11-04 19:24:54 +0800

    modify time

--- lib/gitlab_git/compare.rb
+++ lib/gitlab_git/compare.rb
@@ -1,7 +1,7 @@
 module Gitlab
   module Git
     class Compare
-      TIME_OUT_TIME = 30
+      TIME_OUT_TIME = 240
       attr_reader :commits, :diffs, :same, :timeout_diffs, :timeout_commits, :head, :base
 
       def initialize(repository, base, head)
@@ -30,7 +30,7 @@ module Gitlab
         if @commits.empty? && @timeout_commits == false
           begin
             Timeout.timeout(TIME_OUT_TIME) do 
-              @commits = Gitlab::Git::Commit.between(repository, @base.id, @head.id)
+              @commits = Gitlab::Git::Commit.between(@repository, @base.id, @head.id)
             end
           rescue Timeout::Error => ex
             @commits = []
