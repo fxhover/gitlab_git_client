f9a6c2ed54f22ca1d39ea66b58db38fbd30d50ad
liuhq002 <yehaha001@163.com>
2014-11-04 15:44:03 +0800

    add compare timeout

--- lib/gitlab_git/compare.rb
+++ lib/gitlab_git/compare.rb
@@ -1,13 +1,15 @@
 module Gitlab
   module Git
     class Compare
-      attr_reader :commits, :diffs, :same, :timeout, :head, :base
+      TIME_OUT_TIME = 30
+      attr_reader :commits, :diffs, :same, :timeout_diffs, :timeout_commits, :head, :base
 
       def initialize(repository, base, head)
         @commits, @diffs = [], []
         @same = false
         @repository = repository
-        @timeout = false
+        @timeout_diffs = false
+        @timeout_commits = false
 
         return unless base && head
 
@@ -20,23 +22,37 @@ module Gitlab
           @same = true
           return
         end
+      end
 
-        @commits = Gitlab::Git::Commit.between(repository, @base.id, @head.id)
+      def commits
+        return [] if @same
+
+        if @commits.empty? && @timeout_commits == false
+          begin
+            Timeout.timeout(TIME_OUT_TIME) do 
+              @commits = Gitlab::Git::Commit.between(repository, @base.id, @head.id)
+            end
+          rescue Timeout::Error => ex
+            @commits = []
+            @timeout_commits = true
+          end
+        end
+        @commits
       end
 
       def diffs(paths = nil)
-        unless @head && @base
-          return []
-        end
+        return [] if @same
 
         # Try to collect diff only if diffs is empty
         # Otherwise return cached version
-        if @diffs.empty? && @timeout == false
+        if @diffs.empty? && @timeout_diffs == false
           begin
-            @diffs = Gitlab::Git::Diff.between(@repository, @head.id, @base.id, *paths)
-          rescue Gitlab::Git::Diff::TimeoutError => ex
+            Timeout.timeout(TIME_OUT_TIME) do
+              @diffs = Gitlab::Git::Diff.between(@repository, @head.id, @base.id, *paths)
+            end
+          rescue Timeout::Error => ex
             @diffs = []
-            @timeout = true
+            @timeout_diffs = true
           end
         end
 
